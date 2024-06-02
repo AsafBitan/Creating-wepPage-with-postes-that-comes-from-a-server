8535247724a8d6631f9ea7cd81150321d99281f3
AsafBitBGU <asafbit@post.bgu.ac.il>
2024-06-02 17:29:21 +0300

    mosty done Co-authored-by: BenjiBGU <BenjiBGU@users.noreply.github.com>

--- hw1/app/navigationBar.js
+++ hw1/app/navigationBar.js
@@ -1,34 +1,9 @@
-'use client'; 
 import styles from "./page.module.css";
-import {useEffect, useState } from "react";
-import axios from 'axios';
+import { useState } from "react";
 
-const POSTS_PER_PAGE = 10;
-const NOTES_URL = 'http://localhost:3001/notes'; 
+const [activePage, setActivePage] = useState(1);
 
-export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const [activePage, setActivePage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  
-  useEffect(() => {
-    const promise = axios.get(NOTES_URL, {
-        params: {
-          _page: activePage,
-          _per_page: POSTS_PER_PAGE
-        }});
-    promise.then(response => { 
-        if (!response.data){
-          throw new Error('No data found');
-        }
-        setPosts(response.data);
-        setTotalPages(Math.ceil(response.headers['x-total-count']/POSTS_PER_PAGE))
-    }).catch(error => { console.log("Encountered an error:" + error)});
-  });
-
-  const pages = Array.from({length: totalPages}, (_, i) => i+1);
-  
-  function goToFirstPage(){
+function goToFirstPage(){
     setActivePage(1);
   };
 
@@ -48,27 +23,12 @@ export default function Home() {
     setActivePage(totalPages)
   };
 
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        {posts.map(post => (
-          <div key={post.id} className={styles.post}>
-            <div className={styles.postHeader}>
-            <h1>{post.title}</h1>
-            <p>Author: {post.author.name}</p>
-            <p>Email: {post.author.email}</p>
-            </div>
-            <div className={styles.postContent}>
-              <p>
-                {post.content}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
-      
-      <div>
-      <button className={styles.button} name="first" onClick={goToFirstPage}>First</button>
+
+
+const NavBar = ({pages, activePage, totalPages}) => {
+    return(
+        <div>
+        <button className={styles.button} name="first" onClick={goToFirstPage}>First</button>
         <button className={styles.button} name="previous" onClick={goToPrevPage}>Previous</button>
         {(totalPages <= 5 || activePage === 1 || activePage === 2) ? 
                 pages.map(i => (i <= 5) ? ( 
@@ -99,7 +59,10 @@ export default function Home() {
         
       <button className={styles.button} name="next" onClick={goToNextPage}>Next</button>
       <button className={styles.button} name="last" onClick={goToLastPage}>Last</button>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default NavBar;
+    
+
