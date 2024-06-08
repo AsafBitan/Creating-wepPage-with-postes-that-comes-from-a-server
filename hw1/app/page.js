b35e2b1e47dc34b702b279c300994836a2faa815
AsafBitBGU <asafbit@post.bgu.ac.il>
2024-06-08 15:06:11 +0300

    Updated with comments and pritter format

--- hw1/app/page.js
+++ hw1/app/page.js
@@ -1,12 +1,12 @@
-'use client'; 
+"use client";
 import styles from "./page.module.css";
-import {useEffect, useState } from "react";
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import NavBar from "./navigationBar";
 
 const POSTS_PER_PAGE = 10;
-const NOTES_URL = 'http://localhost:3001/notes'; 
-
+const NOTES_URL = "http://localhost:3001/notes";
+// How the post will look.
 const Post = ({ post }) => (
   <div className={styles.post}>
     <div className={styles.postHeader}>
@@ -14,41 +14,60 @@ const Post = ({ post }) => (
       <p>Author: {post.author.name}</p>
       <p>Email: {post.author.email}</p>
     </div>
-    <div className={styles.postContent}> 
+    <div className={styles.postContent}>
       <p>{post.content}</p>
     </div>
   </div>
 );
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const [activePage, setActivePage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  
+  const [posts, setPosts] = useState([]); // Setting the posts that are going to show.
+  const [activePage, setActivePage] = useState(1); // Setting the active page we are going to be on.
+  const [totalPages, setTotalPages] = useState(1); // Setting the total pages we will have.
+
+  // Connencting to the server and getting the relevant information.
   useEffect(() => {
     const promise = axios.get(NOTES_URL, {
-        params: {
-          _page: activePage,
-          _per_page: POSTS_PER_PAGE
-        }});
-    promise.then(response => { 
-        if (!response.data){
-          throw new Error('No data found');
+      params: {
+        _page: activePage,
+        _per_page: POSTS_PER_PAGE,
+      },
+    });
+    promise
+      .then((response) => {
+        if (!response.data) {
+          throw new Error("No data found");
         }
         setPosts(response.data);
-        setTotalPages(Math.ceil(response.headers['x-total-count']/POSTS_PER_PAGE))
-    }).catch(error => { console.log("Encountered an error:" + error)});
+        setTotalPages(
+          Math.ceil(response.headers["x-total-count"] / POSTS_PER_PAGE) // The total pages will be the number of total posts we have devided by the number of posts per page
+        );
+      })
+      .catch((error) => {
+        console.log("Encountered an error:" + error);
+      });
   });
 
   const currentFivePages = (totalPages, activePage) => {
-    if (activePage <= 2 || totalPages <= 5){
-      return Array.from({length: Math.min(totalPages, 5)}, (_, i) => i+1);
-    }
-    else if (activePage >= (totalPages -2)){
-      return [totalPages-4,totalPages-3,totalPages-2,totalPages-1,totalPages];
-    }
-    else {
-      return [activePage-2, activePage-1, activePage, activePage+1, activePage+2];
+    // Desiding what buttons to other pages to show depending on the total pages and the active page
+    if (activePage <= 2 || totalPages <= 5) {
+      return Array.from({ length: Math.min(totalPages, 5) }, (_, i) => i + 1);
+    } else if (activePage >= totalPages - 2) {
+      return [
+        totalPages - 4,
+        totalPages - 3,
+        totalPages - 2,
+        totalPages - 1,
+        totalPages,
+      ];
+    } else {
+      return [
+        activePage - 2,
+        activePage - 1,
+        activePage,
+        activePage + 1,
+        activePage + 2,
+      ];
     }
   };
 
@@ -57,17 +76,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        {posts.map(post => (
-            <Post key={post.id} post={post} />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
         ))}
       </div>
-      
+      {/* NavBar will handel all the navagation and its layout */}
       <NavBar
         pages={pages}
         activePage={activePage}
         totalPages={totalPages}
         setActivePage={setActivePage}
-        />
+      />
     </main>
   );
 }
